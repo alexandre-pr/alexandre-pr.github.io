a2800a0566bb21641331967000aa0e1a97f3bb29
alexandre-pr <alexandre-perot@orange.fr>
2022-05-21 22:53:36 +0200

    wip

--- js/header.js
+++ js/header.js
@@ -11,3 +11,33 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+const aboutBlock = document.getElementById('about')
+
+function setHeader () {
+    const scrollPoint = window.pageYOffset + headerHeight
+  
+    let blockPoint = 61 - (scrollPoint - secondBlock.offsetTop)
+    if (blockPoint <= 0) { blockPoint = 0 }
+  
+    if (scrollPoint > secondBlock.offsetTop) {
+      header[0].style = `max-height: ${blockPoint}px;`
+    } else {
+      header[0].style = `max-height: ${headerHeight}px;`
+    }
+    window.requestAnimationFrame(setHeader)
+  }
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    console.log('Top: ' + top);
+            var header = document.getElementsByTagName('header');
+            var offset = header.innerHeight; //changed offset to be dynamic, so it works on mobile screens.
+    if(top > offset){
+        header[0].classList.remove('top');
+        header[0].classList.add('scrolled');
+    } else {
+                header[0].classList.remove('scrolled');
+                header[0].classList.add('top');
+            }
+};
