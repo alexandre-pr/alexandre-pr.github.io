1b36722fc6ddc3292479ba61e271ca9f6a0ffdb0
alexandre-pr <alexandre-perot@orange.fr>
2022-05-21 23:25:55 +0200

    header working

--- js/header.js
+++ js/header.js
@@ -11,18 +11,3 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
-
-const aboutBlock = document.getElementById('about')
-
-
-window.onscroll = function(){
-    var top = window.scrollY;
-    console.log('Top: ' + top);
-            var header = document.getElementsByTagName('header');
-            var offset = header.innerHeight; //changed offset to be dynamic, so it works on mobile screens.
-    if(top > aboutBlock.offsetTop){
-        document.body.classList.add('light-background');
-    } else {
-        document.body.classList.remove('light-background');
-            }
-};
--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ $(function(){
     $("#loaded_header").load("header.html"); 
     $("#loaded_footer").load("footer.html"); 
 });
+
+const aboutBlock = document.getElementById('about');
+const skillsBlock = document.getElementById('skills');
+const educationBlock = document.getElementById('education');
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    console.log('Top: ' + top);
+    if((top > aboutBlock.offsetTop && top<skillsBlock.offsetTop)||(top>educationBlock.offsetTop)){
+        document.body.classList.add('light-background');
+    } else {
+        document.body.classList.remove('light-background');
+            }
+};
