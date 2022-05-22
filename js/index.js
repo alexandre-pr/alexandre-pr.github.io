8b5c30f881ceea8c52e0e5cc3a2c8b0a91981647
alexandre-pr <alexandre-perot@orange.fr>
2022-05-22 23:23:22 +0200

    wip

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-
-
-$(function(){
-    $("#loaded_header").load("header.html"); 
-    $("#loaded_footer").load("footer.html"); 
-    $("#loaded_header-portfolio-item").load("portfolio-item-header.html"); 
-});
-
 const aboutBlock = document.getElementById('about');
 const skillsBlock = document.getElementById('skills');
 const educationBlock = document.getElementById('education');
@@ -44,29 +36,3 @@ window.onscroll = function(){
         }
     });
 };
-
-function watchForHover() {
-    // lastTouchTime is used for ignoring emulated mousemove events
-    let lastTouchTime = 0
-  
-    function enableHover() {
-      if (new Date() - lastTouchTime < 500) return
-      document.body.classList.remove('no-hover')
-    }
-  
-    function disableHover() {
-      document.body.classList.add('no-hover')
-    }
-  
-    function updateLastTouchTime() {
-      lastTouchTime = new Date()
-    }
-  
-    document.addEventListener('touchstart', updateLastTouchTime, true)
-    document.addEventListener('touchstart', disableHover, true)
-    document.addEventListener('mousemove', enableHover, true)
-  
-    enableHover()
-  }
-  
-watchForHover()
