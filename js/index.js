dbb44ccaff0affba20bc3202e76e77875e3f2d59
alexandre-pr <alexandre-perot@orange.fr>
2022-05-22 16:25:52 +0200

    wip

--- js/index.js
+++ js/index.js
@@ -43,3 +43,29 @@ window.onscroll = function(){
         }
     });
 };
+
+function watchForHover() {
+    // lastTouchTime is used for ignoring emulated mousemove events
+    let lastTouchTime = 0
+  
+    function enableHover() {
+      if (new Date() - lastTouchTime < 500) return
+      document.body.classList.remove('no-hover')
+    }
+  
+    function disableHover() {
+      document.body.classList.add('no-hover')
+    }
+  
+    function updateLastTouchTime() {
+      lastTouchTime = new Date()
+    }
+  
+    document.addEventListener('touchstart', updateLastTouchTime, true)
+    document.addEventListener('touchstart', disableHover, true)
+    document.addEventListener('mousemove', enableHover, true)
+  
+    enableHover()
+  }
+  
+watchForHover()
