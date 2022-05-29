cc3873284b98ce4243b5bdd1cb520d8ad6e2c59e
alexandre-pr <alexandre-perot@orange.fr>
2022-05-29 16:27:16 +0200

    domos page done

--- js/portfolio-item.js
+++ js/portfolio-item.js
@@ -3,3 +3,4 @@ document.querySelectorAll('image-w-description__video').defaultPlaybackRate = 4.
 Array.prototype.forEach.call(document.getElementsByClassName('speed2'), function(video) {
     video.playbackRate = 2.0;
 });
+
