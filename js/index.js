3f77121911d01127957a40df8a6017ceba634bc0
PÉROT Alexandre <alexandre.perot.auditeur@lecnam.net>
2022-11-14 02:04:57 +0100

    Filter projects option

--- js/index.js
+++ js/index.js
@@ -9,6 +9,15 @@ const domosBlock = document.getElementById('domos');
 const sunkpearlBlock = document.getElementById('sunkpearl');
 const illustrationsBlock = document.getElementById('illustrations');
 
+// Add active class to the current control button (highlight it)
+
+var filteredElements = document.getElementById("work").getElementsByClassName("filter-div");;
+var btns = document.getElementsByClassName("filter-btn");
+var lastActive = document.getElementsByClassName("filter-btn--active")[0];
+var portfolio = document.getElementById("work-portfolio");
+animDuration=500;
+portfolio.style.animationDuration = "250ms";
+
 
 function HandleScrollPorftolioWork(){
     var headerBlock = document.getElementsByClassName('header__logos')[0];
@@ -50,3 +59,67 @@ function HandleScrollPorftolioWork(){
 };
 
 window.addEventListener("scroll", HandleScrollPorftolioWork, true);
+
+
+// Work filter
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var i;
+  if (c == "all") c = "";
+  
+  setTimeout(() => {FadeIn(c)}, animDuration/2);
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+//   for (i = 0; i < filteredElements.length; i++) {
+//     RemoveClass(filteredElements[i], "filter-div--show");
+//     if (filteredElements[i].className.indexOf(c) > -1) AddClass(filteredElements[i], "filter-div--show");
+//   }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    lastActive.className = lastActive.className.replace("filter-btn--active", "");
+    this.className += " filter-btn--active";
+    if(lastActive != this){
+        portfolio.style.animationName = "fade-out";
+    }
+    lastActive = this;
+  });
+}
+
+function FadeIn(c){
+    portfolio.offsetHeight;
+    portfolio.style.animationName = "fade-in";
+    for (i = 0; i < filteredElements.length; i++) {
+        RemoveClass(filteredElements[i], "filter-div--show");
+        if (filteredElements[i].className.indexOf(c) > -1) AddClass(filteredElements[i], "filter-div--show");
+    }
+}
